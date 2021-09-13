bf9220b80f15cf8a55cd9fa5cda49bbbb11c3e19
GioAbuladze-Creator <misho6344@gmail.com>
2021-09-13 06:27:18 +0400

    Initial commit

--- script.js
+++
@@ -1,6 +0,0 @@
-let requests=new XMLHttpRequest();
-requests.addEventListener('load',function(){
-    console.log(this,this.responseText)
-});
-requests.open('GET','https://jsonplaceholder.typicode.com/posts')
-requests.send();
